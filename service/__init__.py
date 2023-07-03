ebfbfbb6f0b98bdee832b0bbb8f6c9cabe0edcf8
sergioAtos <sergio.salmeron@atos.net>
2023-07-03 12:33:37 -0400

    format fix

--- service/__init__.py
+++ service/__init__.py
@@ -19,7 +19,7 @@ app.config.from_object(config)
 talisman = Talisman(app)
 
 # Create CORS
-CORS(app) 
+CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
